a7a3a856842d6e8c3f0b83bd453b02e6394da53c
Larissa Gomes Silva <larissagomes2silva@gmail.com>
2023-10-30 23:14:33 -0300

    add: adicionando build

--- Assets/Scripts/Market.cs
+++ Assets/Scripts/Market.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Search;
-using System;
-using System.Diagnostics;
 
 public class Market : MonoBehaviour
 {
